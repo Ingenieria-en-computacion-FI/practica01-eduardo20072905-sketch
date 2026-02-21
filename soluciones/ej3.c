362c3347865d6e1f061478a50f5eaac1f08600b2
eduardo20072905-sketch <eduardo.20072905@gmail.com>
2026-02-21 14:29:53 -0600

    Ejercicio 3 modificado

--- soluciones/ej3.c
+++ soluciones/ej3.c
@@ -1,6 +1,25 @@
-
 #include <stdio.h>
-int main(){
-    // Resolver ejercicio 3
+
+int WinMain() {
+
+    int a[3];
+    int b[3];
+    int c[3];
+    int i,j,k;
+
+    for ( i= 0; i < 3; i++)
+    {
+    printf("&a[%d]=%p\n",i,&a[i]);
+    }
+    for ( j= 0; j < 3; j++)
+    {
+    printf("&b[%d]=%p\n",j,&b[j]);  //Los arreglos no son consecutivos, pero sus elemenos si lo son
+    }
+    for ( k= 0; k < 3; k++)         //de si pertenecen a "a" o "b"
+    {
+    printf("&c[%d]=%p\n",k,&c[k]);
+    }
+
     return 0;
+    
 }
