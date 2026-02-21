df65dc58e325c944cb2609915f675a9d27854824
eduardo20072905-sketch <eduardo.20072905@gmail.com>
2026-02-21 14:26:26 -0600

    Ejercicio 11 modificado
    Added comments explaining the exercise and data types.

--- soluciones/ej11.c
+++ soluciones/ej11.c
@@ -1,6 +1,13 @@
 
-#include <stdio.h>
-int main(){
-    // Resolver ejercicio 11
-    return 0;
-}
+//&a[0]=1000
+//&a[1]=1008
+//&a[2]=1016
+
+//determina el tamaño del tipo:
+//El tamaño del tipo es de 8 bytes
+
+//tipo posible: 
+//double, long long int, etc. ya que estos tipos de datos suelen ocupar 8 bytes en la mayoría de las plataformas.
+
+//formula usada:
+//&a[i]=&a[0]+i*tamaño del tipo
